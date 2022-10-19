c191ce867d90a38e307b9a92957b116db0e58254
FebriTahta <febrirtah@gmail.com>
2022-10-19 10:42:51 +0700

    Perbaikan tinggi dan besar cetak pelanggan

--- resources/views/cetak/cetak_pelanggan.blade.php
+++ resources/views/cetak/cetak_pelanggan.blade.php
@@ -14,25 +14,25 @@
 <body>
     <div class="cetak" style="margin-left: 80px">
         <div class="kop" style="line-height: 23px;">
-            <table style="font-size: 17px; font-weight: 700">
+            <table style="font-size: 16px; font-weight: 700">
                 <tr>
-                    <td><b>KEPADA Yth</b></td>
+                    <td><b>KEPADA YTH.</b></td>
                 </tr>
                 <tr>
-                    <td><b>Ust / Ustd. {{$data->kepala}}</b></td>
+                    <td style="text-transform: uppercase"><b>UST / USTD. {{$data->kepala}}</b></td>
                 </tr>
                 <tr>
-                    <td><b>Telp. {{$data->telepon}}</b></td>
+                    <td><b>TElP. {{$data->telepon}}</b></td>
                 </tr>
                 <tr>
                     @if (strlen($data->alamat) < 60 || strlen($data->alamat) == 60)
-                    <td style="width: 50%"><b style="text-transform: uppercase" style="width: 50%">
+                    <td style="width: 50%"><b style="text-transform: capitalize" style="width: 50%">
                         {{substr($data->alamat,0,50)}}</b> <br> <b>{{substr($data->alamat,50)}}</b>
                         {{-- {{$data->alamat}} --}}
                     </td>
                     <td style="width: 50%"></td>
                     @else
-                    <td style="width: 68%"><b style="text-transform: uppercase">{{$data->alamat}}</b></td>
+                    <td style="width: 68%"><b style="text-transform: capitalize">{{$data->alamat}}</b></td>
                     <td style="width: 38%"></td>
                     @endif
                 </tr>
@@ -57,7 +57,7 @@
                     <td><b>Jl. Ketintang Timur PTT VB Surabaya</b></td>
                 </tr>
                 <tr>
-                    <td style="width: 70%"><b style="text-transform: uppercase">Telp. 031-8281278 / 0856 0796 0078</b></td>
+                    <td style="width: 70%"><b style="text-transform: capitalize">Telp. 031-8281278 / 0856 0796 0078</b></td>
                     <td style="width: 30%"></td>
                 </tr> 
                 
